33af5ff358aeb69bd58cae57ed35c22dbd880417
magcodes <70335377+magcodes@users.noreply.github.com>
2021-10-29 15:09:49 +0000

    Human Class

--- src/barmanagement/Human.java
+++ src/barmanagement/Human.java
@@ -10,8 +10,66 @@ package barmanagement;
  * @author Mary Denkyiwaa
  */
 public class Human {
-    String William;
-    String Spectrum;
+    public final String firstName;
+    public String nickName;
+    private int wallet;
+    private int popularityRating;
+    private String meaningfulCry;  
+
+    public Human(String firstName, String nickName, int wallet, int popularityRating, String meaningfulCry) {
+        this.firstName = firstName;
+        this.nickName = nickName;
+        this.wallet = wallet;
+        this.popularityRating = popularityRating;
+        this. meaningfulCry = meaningfulCry;
+        
+    }
+
+    public void speak() {
+        
+    }
+    
+    public void drink() {
+        
+    }
+     
+     public void pay() {
+        
+    }
+     
+    public void offerDrink() {
+        
+    }
+     
+    public void present() {
+        
+    }
+    
+    public int getWallet() {
+        return wallet;
+    }
+    
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
     
     
+    public int getPopularityRating(){
+        return popularityRating;
+    }
+    
+    public void setPopularityRating(int popularityRating) {
+        this.popularityRating = popularityRating;
+    }
+     
+    public String getMeaningfulCry() {
+        return meaningfulCry;
+    }
+    
+    public void setMeaningfulCry(String meaningfulCry) {
+        this.meaningfulCry = meaningfulCry;
+    }
+     
+     
+     
 }
