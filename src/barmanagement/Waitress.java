a0d1d835ca4bfcccf9acce8108acf673fa1ae952
magcodes <70335377+magcodes@users.noreply.github.com>
2021-11-01 11:24:22 +0000

    Server, Supplier, Waiter, Waitress Classes

---
+++ src/barmanagement/Waitress.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barmanagement;
+
+/**
+ *
+ * @author Mary Denkyiwaa
+ */
+public class Waitress extends Server {
+    
+     public Waitress(String firstName, String nickName, int wallet, int popularityRating, String meaningfulCry) {
+        super(firstName, nickName, wallet, popularityRating, meaningfulCry);
+         
+        
+     }
+     
+     public void coefficientOfCharm() {
+         
+     }
+}
+
